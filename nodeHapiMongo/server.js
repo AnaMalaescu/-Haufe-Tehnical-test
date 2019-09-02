f9c8e89b0e5d8374d829203f7544e843c6a4e340
Marian Iriza <marian_iriza@netex.ro>
2019-09-02 14:21:06 +0300

    added in server.js the connection to mongoDB

--- nodeHapiMongo/server.js
+++ nodeHapiMongo/server.js
@@ -1,46 +1,33 @@
-const Hapi = require('hapi');
-const inert = require('inert');
+console.log('Server-side code running');
 
-const server = new Hapi.Server({
-    host: 'localhost',
-    port: 3101,
+const express = require('express');
+
+const mongodb = require('mongodb').MongoClient;
+
+const app = express();
+
+// serve files from the public directory
+app.use(express.static('public'));
+
+// connect to the db and start the express server
+let db;
+
+// ***Replace the URL below with the URL for your database***
+const url =  'mongodb://127.0.0.1:27017/history';
+// const url =  'mongodb://localhost:21017/databaseName';
+
+mongodb.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 3101
+  app.listen(3101, () => {
+    console.log('listening on 3101');
+  });
 });
 
-// add each route
-server.route([
-    {
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return 'I am the home route';
-        },
-    },
-    {
-        method: 'GET',
-        path: '/example',
-        handler: (request, h) => {
-            return { msg: 'I am json' };
-        },
-    },
-    {
-        method: 'GET',
-        path: '/mongoDB',
-        handler: (request, h) => {
-            return h.file('./main.html')
-        },
-    }
-]);
-
-// define server start function
-const launch = async () => {
-    try {
-        await server.register(inert);
-        await server.start();
-    } catch (err) {
-        console.error(err);
-        process.exit(1);
-    };
-    console.log(`Server running at ${server.info.uri}`);
-}
-
-launch();
+// serve the homepage
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/main.html');
+});
