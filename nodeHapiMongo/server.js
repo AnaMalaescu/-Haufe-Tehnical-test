1befb5b0ac899b7d9ecac98bb9c1afc987194a5d
Marian Iriza <marian_iriza@netex.ro>
2019-09-02 10:37:28 +0300

    try to put a html page in server.js'

---
+++ nodeHapiMongo/server.js
@@ -0,0 +1,43 @@
+const Hapi = require('hapi');
+const server = new Hapi.Server({
+    host: 'localhost',
+    port: 3101,
+});
+
+// add each route
+server.route([
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'I am the home route';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/example',
+        handler: (request, h) => {
+            return { msg: 'I am json' };
+        },
+    },
+    {
+        method: 'GET',
+        path: '/mongoDB',
+        handler: (request, h) => {
+            return h.file('main.html');
+        },
+    }
+]);
+
+// define server start function
+const launch = async () => {
+    try {
+        await server.start();
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    };
+    console.log(`Server running at ${server.info.uri}`);
+}
+
+launch();
