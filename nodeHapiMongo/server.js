2fd72eedb17139ba89f7935fe40bf60e555b3c20
Marian Iriza <marian_iriza@netex.ro>
2019-09-02 22:06:14 +0300

    added the post dummy data generation and save it to the mongoDB

--- nodeHapiMongo/server.js
+++ nodeHapiMongo/server.js
@@ -15,6 +15,8 @@ app.set("view engine", "ejs");
 // serve files from the public directory
 app.use(express.static('public'));
 
+app.use(bodyparser.json());
+
 // connect to the db and start the express server
 let db;
 
@@ -72,25 +74,14 @@ app.get("/getdataconn", function (req, res) {
 
 });
 
-//app.post('/clicked', (req, res) => {
+app.post('/addrules', (req,res) => {
 
-  //var dbo = db.db("history");
-  //dbo.collection("rulers").findOne({}, function(err, result)
-    //{
-    //  if (err) throw err;
-      //console.log(result.name);
-      //console.log(result.country);
-      //db.close();
-    //});
-//  res.sendStatus(201);
-//});
+  var data = new detailsModel(req.body);
+  data.save().then(item=> {
+    res.send("item saved to database");
+  })
+  .catch(err => {
+    res.status(400).send("unable to save to mongoDB");
+  });
 
-// get the click data from the database
-//app.get('/clicks', (req, res) => {
-
-//  var dbo = db.db("history");
-  //dbo.collection('rulers').find().toArray((err, result) => {
-  //  if (err) return console.log(err);
-    //res.send(result);
-  //});
-//});
+});
